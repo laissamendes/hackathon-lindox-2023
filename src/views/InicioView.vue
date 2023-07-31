d9d65fcbfa2609ba2360cded80873f9680e350fb
Laíssa Mendes <laissa.mendes2006@gmail.com>
2023-07-31 17:00:24 -0300

    filnamente o hover nas imagens de serviço foi concluido

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,47 +1,90 @@
 <script setup>
+import { ref } from 'vue'
+
+const maquina = ref(false)
+
+function toggleMaquina() {
+
+  maquina.value = !maquina.value
+  console.log(maquina.value)
+}
+const maqLavar = ref(false)
+
+function toggleMaqLavar() {
+
+  maqLavar.value = !maqLavar.value
+  console.log(maqLavar.value)
+}
+const calca = ref(false)
+
+function toggleCalca() {
+
+  calca.value = !calca.value
+  console.log(calca.value)
+}
+const produto = ref(false)
+
+function toggleProduto() {
+
+  produto.value = !produto.value
+  console.log(produto.value)
+}
+
 </script>
 
 <template>
-  <header > 
+  <header>
     <div class="menu">
-    <router-link to="/">INÍCIO | </router-link>
+      <router-link to="/">INÍCIO | </router-link>
       <router-link to="/lojas">LOJAS | </router-link>
       <router-link to="/lavanderia">LAVANDERIA | </router-link>
       <router-link to="/costura">COSTURA | </router-link>
       <router-link to="/tingimento">TINGIMENTO | </router-link>
       <router-link to="/produtos">PRODUTOS | </router-link>
-      <router-link to="/sacola"><img src="sacola.png" alt="" width="95" height="50"></router-link>
-  </div>
-   </header>
+      <router-link to="/sacola"><img src="sacola.png" alt="" widthmouseout="95" height="50"></router-link>
+    </div>
+  </header>
 
   <div id="paginaInicial">
-  <div id="titulo">
-    <!-- <div > -->
+    <div id="titulo">
+      <!-- <div > -->
       <h1 class="nomeEmpresa">
-        <div class="titulo-part1">Restaura</div> 
-        <div class="titulo-part2">Jeans</div></h1> 
-    <!-- </div> -->
-    <h2 id="slogan">A solução para a sua roupa</h2>
+        <div class="titulo-part1">Restaura</div>
+        <div class="titulo-part2">Jeans</div>
+      </h1>
+      <!-- </div> -->
+      <h2 id="slogan">A solução para a sua roupa</h2>
     </div>
-      
-      <div class="informacoes">
-<div class="informacao">
-  <p>A primeira unidade foi criada na cidade de Santa Maria/RS, uma cidade militar e universitária.O cenário perfeito para a criação do pequeno espaço que viria a ser um verdadeiro laboratório para a marca.</p>
-</div>
-<div class="informacao">
-  <p>A Restaura Jeans é uma empresa que trabalha juntamente com a sustentabilidade e contém projetos que visam a conservação do meio ambiente.</p>
-</div>
-<div class ="informacao">
- <p>A empresa também ja conquistou vários prêmios,
-dentre eles, por dez anos consecutivos ganhou o prêmio ABF e também foi reconhecida pela pequenas empresas e grandes negócios.</p> 
-</div>
+    mouseout
+    <div class="informacoes">
+      <div class="informacao">
+        <p>A primeira unidade foi criada na cidade de Santa Maria/RS, uma cidade militar e universitária.O cenário
+          perfeito para a criação do pequeno espaço que viria a ser um verdadeiro laboratório para a marca.</p>
       </div>
-     <div id="servicos">
-<img src="maquina-Sfundo.png" alt="" class="servico">
-<img src="maqLavar-Sfundo.png" alt="" class="servico">
-<img src="calca-Sfundo.png" alt="" class="servico">
-<img src="produto-Sfundo.png" alt="" class="servico">
-</div>
+      <div class="informacao">
+        <p>A Restaura Jeans é uma empresa que trabalha juntamente com a sustentabilidade e contém projetos que visam a
+          conservação do meio ambiente.</p>
+      </div>
+      <div class="informacao">
+        <p>A empresa também ja conquistou vários prêmios,
+          dentre eles, por dez anos consecutivos ganhou o prêmio ABF e também foi reconhecida pela pequenas empresas e
+          grandes negócios.</p>
+      </div>
+    </div>
+    <div id="servicos">
+      <img :src="maquina ? '../../maquina-Cfundo.png' : '../../maquina-Sfundo.png'" alt="" class="servico"
+        @mouseover="toggleMaquina" @mouseleave="toggleMaquina">
+
+      <img :src="maqLavar ? '../../maqLavar-Cfundo.png' : '../../maqLavar-Sfundo.png'" alt="" class="servico"
+        @mouseover="toggleMaqLavar" @mouseleave="toggleMaqLavar">
+
+      <img :src="calca ? '../../calca-Cfundo.png' : '../../calca-Sfundo.png'" alt="" class="servico"
+        @mouseover="toggleCalca" @mouseleave="toggleCalca">
+
+      <img :src="produto ? '../../produto-Cfundo.png' : '../../produto-Sfundo.png'" alt="" class="servico"
+        @mouseover="toggleProduto" @mouseleave="toggleProduto">
+
+    </div>
   </div>
 
   <footer>
@@ -52,57 +95,80 @@ dentre eles, por dez anos consecutivos ganhou o prêmio ABF e também foi reconh
 </template>
 
 <style scoped>
-.servico{
+.servico:hover {
+  cursor: pointer;
+  transform: scale(1.2);
+}
+
+.servico {
   width: 200px;
   height: 150px;
   border-radius: 10px;
   margin: 20px;
+  overflow: hidden;
+  transition: .5s all
 }
+
+#servicos {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  justify-items: center;
+}
+
 .nomeEmpresa {
   display: flex;
   flex-direction: row;
   align-items: center;
   display: flex;
   flex-direction: column;
-justify-content: center;
-align-items: center;
+  justify-content: center;
+  align-items: center;
 }
-#titulo{
+
+#titulo {
   background-color: rgba(255, 255, 255, 0.582);
   align-items: center;
   display: flex;
   flex-direction: column;
-justify-content: center;
-align-items: center;
-text-align: left;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
 }
-.titulo-part1{
+
+.titulo-part1 {
   color: #BB1903;
 }
-.titulo-part2{
+
+.titulo-part2 {
   margin-left: .75rem;
   color: #0A376B;
 }
-#slogan{
+
+#slogan {
   color: #403838;
   text-align: left;
 }
-footer{
+
+footer {
   background-color: #003e8faf;
 
 }
-header{
+
+header {
   background-color: #003e8faf;
 }
-main{
+
+main {
   color: #003d8f;
 }
-.informacoes{
+
+.informacoes {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   justify-items: center;
 }
-.informacao{
+
+.informacao {
   border-color: white;
   border-radius: 20px;
   background-color: white;
@@ -114,5 +180,4 @@ main{
   padding: 15px;
   font-size: 25px;
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.514);
-}
-</style>
+}</style>
